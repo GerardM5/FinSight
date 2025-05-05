b3cd3eaaf5adaa9d9f82ffefd957d6b749f9e984
Gerard <gerartph@gmail.com>
2025-05-05 14:10:33 +0200

    init

---
+++ src/test/java/org/example/finsight/FinSightApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.finsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinSightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
